fc6a3223e1ead043139f5523e0308cea2ef2c8b0
khyati_bardolia <khyati.bardolia@gmail.com>
2020-12-28 16:42:45 +0530

    - minor fix

---
+++ src/api/config/config.ts
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyB1IQNOiACXVuPw7QHvMjjoPahmSrUj__E",
+    authDomain: "react-with--login.firebaseapp.com",
+    projectId: "react-with--login",
+    storageBucket: "react-with--login.appspot.com",
+    messagingSenderId: "633020000135",
+    appId: "1:633020000135:web:cf3a521aa17c108e1c1522",
+    measurementId: "G-YRT98W8M3L"
+};
+export default FIREBASE_CONFIG;
